5270d134cc52573710411c29d55a2947d47c6506
menoudyann <Yann.MENOUD@cpnv.ch>
2024-02-23 18:46:11 +0100

    first commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,33 @@
+import "./login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            condimentum vestibulum nunc, ac rutrum dui dignissim a. Praesent
+            massa nulla, congue consectetur interdum sed, rhoncus vel dolor.
+            Donec dictum, sem.
+          </p>
+          <span>Dont have an account ?</span>
+          <button>Sign Up</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <label>Email</label>
+            <input type="text" placeholder="Enter your email..." />
+            <label>Password</label>
+            <input type="password" placeholder="Enter your password..." />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
